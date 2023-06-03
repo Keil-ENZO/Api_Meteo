d85fda05a07c657c9274e24b4884c031a482169b
Keil-ENZO <enzo.keil06@icloud.com>
2023-06-03 10:37:59 +0200

    class focus ajouter + changement background

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function success(pos) {
 
       tempMax.innerHTML =
         "Max " + data.list[0].main.temp_max.toFixed(0) + " °C";
-      tempMax.style.color = "orange";
+      tempMax.style.color = "#EC6E4C";
 
       vent.innerHTML = "Vent " + data.list[0].wind.speed + " km/h";
       humidite.innerHTML = "Humidite " + data.list[0].main.humidity + " %";
@@ -68,24 +68,47 @@ function success(pos) {
 
       icon.src = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png`;
 
-      // heure.innerHTML = data.list[0].dt_txt.slice(11, 13) + " h";
-      // temperatureHeure.innerHTML = data.list[0].main.temp.toFixed(0) + " °C";
+      //Background celon la meteo
+      if (data.list[0].weather[0].main === "Clear") {
+        document.body.style.backgroundColor = "skyblue";
+      } else if (data.list[0].weather[0].main === "Clouds") {
+        document.body.style.backgroundColor = "rgb(118, 118, 118) ";
+      } else if (data.list[0].weather[0].main === "Rain") {
+        document.body.style.backgroundColor = "rgb(118, 118, 118) ";
+      } else if (data.list[0].weather[0].main === "Snow") {
+        document.body.style.backgroundColor = "rgb(188, 181, 181)";
+      } else if (data.list[0].weather[0].main === "Thunderstorm") {
+        document.body.style.backgroundColor = "rgb(84, 84, 84)";
+      } else if (data.list[0].weather[0].main === "Drizzle") {
+        document.body.style.backgroundColor = "rgb(188, 181, 181)";
+      } else if (data.list[0].weather[0].main === "Mist") {
+        document.body.style.backgroundColor = "rgb(188, 181, 181)";
+      }
 
       //Affichage de la list entiere avec ce code
       const mainContent = document.querySelector(".main-content");
-
       let count = 0;
 
       data.list.forEach((forecast) => {
         if (count < 5) {
           const forecastElement = document.createElement("a");
           forecastElement.setAttribute("href", "#");
-          forecastElement.classList.add("main");
 
-          //Au click sur un element de la liste
+          forecastElement.classList.add("main");
+          if (count === 0) {
+            forecastElement.classList.add("focus");
+          }
+
+          //Au click affichage de la meteo celon la ou on a cliqué
           forecastElement.addEventListener("click", () => {
             console.log(forecast);
-            //affichage de la meteo celon la ou on a cliqué
+
+            const focus = document.querySelector(".focus");
+            if (focus) {
+              focus.classList.remove("focus");
+            }
+            forecastElement.classList.add("focus");
+
             ville.innerHTML = data.city.name;
             dateHeure.innerHTML =
               forecast.dt_txt.slice(8, 10) +
@@ -111,13 +134,30 @@ function success(pos) {
 
             tempMax.innerHTML =
               "Max " + forecast.main.temp_max.toFixed(0) + " °C";
-            tempMax.style.color = "orange";
+            tempMax.style.color = "#EC6E4C";
 
             vent.innerHTML = "Vent " + forecast.wind.speed + " km/h";
             humidite.innerHTML = "Humidite " + forecast.main.humidity + " %";
             pression.innerHTML = "Presssion " + forecast.main.pressure + " hPa";
 
-            icon.src = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`; //icon de change pas
+            icon.src = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`;
+
+            //Background celon la meteo
+            if (forecast.weather[0].main === "Clear") {
+              document.body.style.backgroundColor = "skyblue";
+            } else if (forecast.weather[0].main === "Clouds") {
+              document.body.style.backgroundColor = "rgb(118, 118, 118) ";
+            } else if (forecast.weather[0].main === "Rain") {
+              document.body.style.backgroundColor = "rgb(118, 118, 118) ";
+            } else if (forecast.weather[0].main === "Snow") {
+              document.body.style.backgroundColor = "rgb(188, 181, 181)";
+            } else if (forecast.weather[0].main === "Thunderstorm") {
+              document.body.style.backgroundColor = "rgb(84, 84, 84)";
+            } else if (forecast.weather[0].main === "Drizzle") {
+              document.body.style.backgroundColor = "rgb(188, 181, 181)";
+            } else if (forecast.weather[0].main === "Mist") {
+              document.body.style.backgroundColor = "rgb(188, 181, 181)";
+            }
           });
 
           const heureElement = document.createElement("p");
@@ -127,11 +167,7 @@ function success(pos) {
           const date = document.createElement("p");
           date.id = "date";
           date.textContent =
-            forecast.dt_txt.slice(8, 10) +
-            "/" +
-            forecast.dt_txt.slice(5, 7) +
-            "/" +
-            forecast.dt_txt.slice(0, 4);
+            forecast.dt_txt.slice(8, 10) + "/" + forecast.dt_txt.slice(5, 7);
 
           const iconElement = document.createElement("img");
           iconElement.id = "icon";
@@ -151,7 +187,7 @@ function success(pos) {
 
           mainContent.appendChild(forecastElement);
 
-          count++; // Incrémente le compteur après chaque élément ajouté
+          count++;
         }
       });
     });
